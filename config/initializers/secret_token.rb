f2a0cf29fcf023320302d407541407effedf07ad
arycancer <arycancer@gmail.com>
2013-04-16 23:30:46 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracore::Application.config.secret_token = '9296619ad79930b8a8bf782005bf62f255ef3da846a93b649d715b60c1f33c833166804aa5c43e048831c6c2ecbdc2676012240e74652e7a3aa0aa06a5357e7a'
